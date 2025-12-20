1e43b229fa31d49c9e5343dd0b906ce16fa0c048
Nivas Bidave <nivasbidave2020@gmail.com>
2025-12-20 19:10:11 +0530

    adding live backend url

--- index.js
+++ index.js
@@ -17,19 +17,32 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
+// const corsOptions = {
+//   origin: [
+//      // "http://localhost:5173",
+//     "https://job-poratl-frontend.vercel.app"
+//   ],
+//   credentials: true,
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   allowedHeaders: ["Content-Type", "Authorization"]
+// };
+
+
+
+// app.use(cors(corsOptions));
+
+
+
 const corsOptions = {
-  origin: [
-    //  "http://localhost:5173",
-    "https://job-poratl-frontend.vercel.app"
-  ],
+  origin: "https://job-poratl-frontend.vercel.app",
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 };
 
-
-
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+
 
 app.get("/home", (req, res)=>{
     return res.status(200).json({
